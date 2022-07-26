19bd134ceec5630ae8a9378e2b420023edd81d7a
SStuurman <stuart@lateral.solutions>
2022-07-26 15:24:28 +0200

    fixed a bug

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,18 +4,18 @@ import { GlobalContext } from '../context/GlobalState';
 
 const AddTransaction = () => {
 
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
-  const { addTransaction } = useContext(GlobalContext)
+  const { addTransaction } = useContext(GlobalContext);
 
-  const onSubmit = e =>{
+  const onSubmit = e => {
     e.preventDefault();
 
-    const newTransaction ={
+    const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
-      text: text,
-      amount: amount
+      text,
+      amount: +amount
     }
 
     addTransaction(newTransaction);
@@ -24,7 +24,7 @@ const AddTransaction = () => {
   return (
     <div> 
         <h3>Add new transaction</h3>
-        <form onSubmit={onSubmit()}>
+        <form onSubmit={onSubmit}>
         <div className="form-control">
             <label htmlFor="text">Text</label>
             <input type="text" value={text} onChange={(e) => setText(e.target.value)}  placeholder="Enter text..." />
